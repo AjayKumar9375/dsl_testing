e4595ab5baa7dc1e6e06d70bdc0c081f0ee36725
ajay1 <ajay1@TeamCity>
2024-06-10 19:27:01 +0530

    TeamCity change in 'Importing_elements' project: 'https://github.com/AjayKumar9375/Json_string.git' VCS root was updated

--- .teamcity/ImportingElements/vcsRoots/ImportingElements_HttpsGithubComAjayKumar9375JsonStringGit.kt
+++ .teamcity/ImportingElements/vcsRoots/ImportingElements_HttpsGithubComAjayKumar9375JsonStringGit.kt
@@ -5,7 +5,7 @@ import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 object ImportingElements_HttpsGithubComAjayKumar9375JsonStringGit : GitVcsRoot({
     name = "https://github.com/AjayKumar9375/Json_string.git"
-    url = "https://github.com/AjayKumar9375/Json_string.git"
+    url = "https://github.com/AjayKumar9375/json_repo.git"
     branch = "refs/heads/main"
     branchSpec = "refs/heads/*"
 })
