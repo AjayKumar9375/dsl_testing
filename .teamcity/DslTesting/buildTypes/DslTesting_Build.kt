f65c52e0109427a587ed4f53239564afc449c6db
AjayKumar9375 <93271770+AjayKumar9375@users.noreply.github.com>
2024-06-10 16:47:32 +0530

    Update DslTesting_Build.kt

--- .teamcity/DslTesting/buildTypes/DslTesting_Build.kt
+++ .teamcity/DslTesting/buildTypes/DslTesting_Build.kt
@@ -14,7 +14,7 @@ object DslTesting_Build : BuildType({
         param("location2", "Bsc")
         param("source", "json_source.json")
         param("location1", "PlantModel")
-        param("Organization", "KPIT")
+        param("Organization", "KPIT123")
     }
 
     vcs {
