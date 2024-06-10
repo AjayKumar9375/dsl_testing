841002f4d906cb1e4667c469b314b8ff56daccaa
ajay1 <ajay1@TeamCity>
2024-06-10 19:30:50 +0530

    TeamCity change in 'Importing_elements' project: 'search_value' build configuration settings were updated

--- .teamcity/ImportingElements/buildTypes/ImportingElements_SearchValue.kt
+++ .teamcity/ImportingElements/buildTypes/ImportingElements_SearchValue.kt
@@ -21,6 +21,7 @@ object ImportingElements_SearchValue : BuildType({
     steps {
         python {
             id = "python_runner"
+            enabled = false
             command = file {
                 filename = "main.py"
                 scriptArguments = "--path_to_source_json_file %source% --path_to_destination_json_file %destination% --location_one %location1% --location_two %location2%"
