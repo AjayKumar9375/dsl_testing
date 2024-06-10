dce31ac5d33aa29181d3f85ffbbe447476cc2328
ajay1 <ajay1@TeamCity>
2024-06-10 16:19:59 +0530

    TeamCity change in '<Root project>' project: Cleanup old configuration files

--- .teamcity/_Self/vcsRoots/HttpsGithubComAjayKumar9375scriptGit.kt
+++
@@ -1,8 +0,0 @@
-package _Self.vcsRoots
-
-import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
-
-object HttpsGithubComAjayKumar9375scriptGit : GitVcsRoot({
-    name = "https://github.com/AjayKumar9375/script.git"
-})
