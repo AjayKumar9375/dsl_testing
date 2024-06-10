0d05062002821774e4d851a1ef33d2a17f7daca1
ajay1 <ajay1@TeamCity>
2024-06-10 13:00:08 +0530

    TeamCity change in '<Root project>' project: Synchronization with own VCS root is enabled

---
+++ .teamcity/ImportingElements/buildTypes/ImportingElements_SearchValue.kt
@@ -0,0 +1,36 @@
+package ImportingElements.buildTypes
+
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.python
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+
+object ImportingElements_SearchValue : BuildType({
+    name = "search_value"
+
+    params {
+        param("destination", "json_destination.json")
+        param("source", "Json_source.json")
+        param("value_input", """InputArtif\\PAnimalModel""")
+    }
+
+    vcs {
+        root(ImportingElements.vcsRoots.ImportingElements_HttpsGithubComAjayKumar9375JsonStringGit)
+    }
+
+    steps {
+        python {
+            id = "python_runner"
+            command = file {
+                filename = "search3.py"
+                scriptArguments = "--path_to_source_json_file %source% --path_to_destination_json_file %destination% --location_to_be_searched %value_input%"
+            }
+        }
+    }
+
+    triggers {
+        vcs {
+            enabled = false
+            branchFilter = ""
+        }
+    }
+})
