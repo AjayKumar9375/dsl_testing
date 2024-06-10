7dc71d52ee96457a38315d29fabaca95e98c2f70
ajay1 <ajay1@TeamCity>
2024-06-10 15:10:26 +0530

    TeamCity change in '<Root project>' project: Synchronization with own VCS root is enabled

---
+++ .teamcity/DslTesting/Project.kt
@@ -0,0 +1,15 @@
+package DslTesting
+
+import DslTesting.buildTypes.*
+import DslTesting.vcsRoots.*
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.Project
+
+object Project : Project({
+    id("DslTesting")
+    name = "Dsl Testing"
+
+    vcsRoot(DslTesting_HttpsGithubComAjayKumar9375dslTestingGitRefsHeadsMain)
+
+    buildType(DslTesting_Build)
+})
