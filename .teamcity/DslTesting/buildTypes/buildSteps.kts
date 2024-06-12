fa161706f4062a2563d4ed28ad08dd57f4af3b49
Ajay Kumar <ajaya9375@gmail.com>
2024-06-12 10:21:38 +0530

    created buildsteps file

--- .teamcity/DslTesting/buildTypes/buildSteps.kts
+++ .teamcity/DslTesting/buildTypes/buildSteps.kts
@@ -1,4 +1,3 @@
-
 import jetbrains.buildServer.configs.kotlin.buildSteps.ScriptBuildStep
 
 class BuildSteps {
@@ -10,19 +9,4 @@ class BuildSteps {
         """.trimIndent()
     }
 
-    fun step2() = ScriptBuildStep {
-        name = "Build Step 2"
-        scriptContent = """
-            echo "Executing Build Step 2"
-        """.trimIndent()
-    }
-    
-    fun step3() = ScriptBuildStep {
-        name = "Build Step 3"
-        scriptContent = """
-            echo "Executing Build Step 3"
-        """.trimIndent()
-    }
-    
-    fun allSteps() = listOf(step1(), step2(), step3())
 }
--- .teamcity/DslTesting/buildTypes/buildTypes.kt
+++ .teamcity/DslTesting/buildTypes/buildTypes.kt
@@ -19,7 +19,7 @@ class MyBuildConfiguration : BuildType({
     val buildSteps = BuildSteps()
     
     steps {
-        buildSteps.allSteps().forEach { step(it) }
+        buildSteps.step1()
     }
     
     triggers {
