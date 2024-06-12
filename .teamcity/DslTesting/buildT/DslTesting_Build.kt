5e61c10097d6c6f2909df0718f14d05f6cd92a4a
Ajay Kumar <ajaya9375@gmail.com>
2024-06-12 13:18:30 +0530

    created buildsteps file

--- .teamcity/DslTesting/buildT/DslTesting_Build.kt
+++ .teamcity/DslTesting/buildT/DslTesting_Build.kt
@@ -1,12 +1,16 @@
 import DslTesting.buildT.buildTypes.BuildTypes
 import DslTesting.buildT.buildSteps.BuildSteps
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.python
 import jetbrains.buildServer.configs.kotlin.failureConditions.BuildFailureOnText
 import jetbrains.buildServer.configs.kotlin.failureConditions.failOnText
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 object DslTestingConfig {
     val dslTestingBuild = DslTestingBuild("YourBuildName", "YourArtifactRules")
 
-    val pythonBuildStep = PythonBuildStep1("YourId", "YourFileName", "YourScriptArguments")
+    val pythonBuildStep = PythonBuildStep("YourId", "YourFileName", "YourScriptArguments")
 
     val dslTestingBuildType = dslTestingBuild {
         steps {
--- .teamcity/DslTesting/buildT/buildSteps/BuildSteps.kts
+++ .teamcity/DslTesting/buildT/buildSteps/BuildSteps.kts
@@ -1,8 +1,13 @@
 package DslTesting.buildT.buildSteps
 
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.python
+import jetbrains.buildServer.configs.kotlin.failureConditions.BuildFailureOnText
+import jetbrains.buildServer.configs.kotlin.failureConditions.failOnText
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
-class PythonBuildStep1(id: String, filename: String, scriptArguments: String) {
+class PythonBuildStep(id: String, filename: String, scriptArguments: String) {
     val pythonStep = python {
         this.id = id
         command = file {
