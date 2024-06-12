4d4e565bba4981271b286cab778fdc2c361e8ed5
Ajay Kumar <ajaya9375@gmail.com>
2024-06-12 16:41:41 +0530

    new change

--- .teamcity/DslTesting/Project.kt
+++ .teamcity/DslTesting/Project.kt
@@ -11,6 +11,20 @@ object Project : Project({
 
     vcsRoot(DslTesting_HttpsGithubComAjayKumar9375dslTestingGitRefsHeadsMain)
 
-    buildType(DslTesting_Build)
-    buildType(DslTesting_BuildTypes)
+    buildType(DslTesting_BuildTypes("Build", "json_destination.json", "python_runner", "main.py",  "--path_to_source_json_file %source% --path_to_destination_json_file %destination% --location_one %location1% --location_two %location2%"))
+
+    // buildType.addTo(this)
+    // buildSteps.addTo(this)
+    // buildType(package_build)
+    // buildType(DslTesting_Build2)
+    // buildType(package_build2)
+
+    // sequential {
+    //     buildType(DslTesting_Build)
+    //     parallel{
+    //         buildType(DslTesting_Build2)
+    //         buildType(package_build2)
+    //     }
+    //     buildType(package_build)
+    // }
 })
--- .teamcity/DslTesting/buildTypes/DslTesting_Build.kt
+++ .teamcity/DslTesting/buildTypes/DslTesting_Build.kt
@@ -7,17 +7,21 @@ import jetbrains.buildServer.configs.kotlin.failureConditions.BuildFailureOnText
 import jetbrains.buildServer.configs.kotlin.failureConditions.failOnText
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
-object DslTesting_Build : BuildType({
-    name = "Build"
 
-    artifactRules = "json_destination.json"
+class DslTesting_BuildTypes( name: String, artifactRules: String, id: String, filename: String, scriptArguments: String) : BuildType({
+    this.name = name
+
+    this.artifactRules = artifactRules
 
     params {
         param("destination", "json_destination.json")
         param("location1", "PlantModel")
         param("location2", "Bsc")
+        param("Organization", "KPIT")
         param("source", "json_source.json")
     }
+
+
 
     vcs {
         root(DslTesting.vcsRoots.DslTesting_HttpsGithubComAjayKumar9375dslTestingGitRefsHeadsMain)
@@ -25,16 +29,11 @@ object DslTesting_Build : BuildType({
 
     steps {
         python {
-            id = "python_runner"
+            this.id = id
             command = file {
-                filename = "main.py"
-                scriptArguments = "--path_to_source_json_file %source% --path_to_destination_json_file %destination% --location_one %location1% --location_two %location2%"
+                this.filename = filename
+                this.scriptArguments = scriptArguments
             }
-        }
-    }
-
-    triggers {
-        vcs {
         }
     }
 
@@ -48,8 +47,68 @@ object DslTesting_Build : BuildType({
         }
     }
 
-    features {
-        perfmon {
-        }
-    }
+
 })
+
+
+// class DslTesting_BuildSteps(id: String, filename: String, scriptArguments: String) : BuildSteps {
+//         steps {
+//             python {
+//                 this.id = id
+//                 command = file {
+//                     this.filename = filename
+//                     this.scriptArguments = scriptArguments
+//                 }
+//             }
+//         }
+// }
+
+// object DslTesting_Build : BuildType({
+//     name = "Build"
+
+//     artifactRules = "json_destination.json"
+
+//     params {
+//         param("destination", "json_destination.json")
+//         param("location1", "PlantModel")
+//         param("location2", "Bsc")
+//         param("Organization", "KPIT")
+//         param("source", "json_source.json")
+//     }
+
+//     vcs {
+//         root(DslTesting.vcsRoots.DslTesting_HttpsGithubComAjayKumar9375dslTestingGitRefsHeadsMain)
+//     }
+
+//     steps {
+//         python {
+//             id = "python_runner"
+//             command = file {
+//                 filename = "main.py"
+//                 scriptArguments = "--path_to_source_json_file %source% --path_to_destination_json_file %destination% --location_one %location1% --location_two %location2%"
+//             }
+//         }
+//     }
+
+//     triggers {
+//         vcs {
+//             // branchFilter = ".*"
+//         }
+//     }
+
+
+//     failureConditions {
+//         failOnText {
+//             conditionType = BuildFailureOnText.ConditionType.CONTAINS
+//             pattern = "Key not found"
+//             failureMessage = "Key didn't match"
+//             reverse = false
+//             stopBuildOnFailure = true
+//         }
+//     }
+
+//     features {
+//         perfmon {
+//         }
+//     }
+// })
