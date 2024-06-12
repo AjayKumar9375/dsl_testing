407143d2a3ac935fda46ea10972451cdbddd0c43
Ajay Kumar <ajaya9375@gmail.com>
2024-06-12 11:18:27 +0530

    created buildsteps file

--- .teamcity/DslTesting/buildTypes/buildTypes.kt
+++ .teamcity/DslTesting/buildTypes/buildTypes.kt
@@ -1,6 +1,5 @@
 package DslTesting.buildTypes
 
-// import buildSteps
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.python
@@ -14,6 +13,16 @@ class DslTesting_Build( name: String, artifactRules: String, id: String, filenam
 
     this.artifactRules = artifactRules
 
+    params {
+        param("destination", "json_destination.json")
+        param("location1", "PlantModel")
+        param("location2", "Bsc")
+        param("Organization", "KPIT")
+        param("source", "json_source.json")
+    }
+
+
+
     vcs {
         root(DslTesting.vcsRoots.DslTesting_HttpsGithubComAjayKumar9375dslTestingGitRefsHeadsMain)
     }
@@ -25,6 +34,15 @@ class DslTesting_Build( name: String, artifactRules: String, id: String, filenam
                 this.filename = filename
                 this.scriptArguments = scriptArguments
             }
+        }
+    }
+    failureConditions {
+        failOnText {
+            conditionType = BuildFailureOnText.ConditionType.CONTAINS
+            pattern = "Key not found"
+            failureMessage = "Key didn't match"
+            reverse = false
+            stopBuildOnFailure = true
         }
     }
 
