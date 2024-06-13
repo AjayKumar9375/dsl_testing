54649649fc2d9535b4a34539fffbd4a66e60f5f9
Ajay Kumar <ajaya9375@gmail.com>
2024-06-13 11:27:19 +0530

    new change

--- .teamcity/DslTesting/Project.kt
+++ .teamcity/DslTesting/Project.kt
@@ -10,7 +10,7 @@ object Project : Project({
     name = "Dsl Testing"
 
     vcsRoot(DslTesting_HttpsGithubComAjayKumar9375dslTestingGitRefsHeadsMain)
-
+ val bts = sequential{
     buildType(DslTesting_BuildTypes(
         "Build", 
         "This build is used for importing the json values",
@@ -18,7 +18,14 @@ object Project : Project({
         "python_runner", 
         "main.py",  
         "--path_to_source_json_file %source% --path_to_destination_json_file %destination% --location_one %location1% --location_two %location2%"))
+ }.buildTypes()
 
+ bts.forEach{ buildType(it)}
+ bts.last().triggers{
+    vcs{
+        
+    }
+ }
     // buildType.addTo(this)
     // buildSteps.addTo(this)
     // buildType(package_build)
