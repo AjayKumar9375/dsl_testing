7ace5f45e28809427e92797a59350d9eb7ab946a
Ajay Kumar <ajaya9375@gmail.com>
2024-06-14 13:07:25 +0530

    new

--- .teamcity/DslTesting/Project.kt
+++ .teamcity/DslTesting/Project.kt
@@ -15,65 +15,74 @@ object Project : Project({
 
 
     //Features List
-    val featureList = listOf(
-        buildType(DslTesting_BuildTypes(
+
+    val feature1 = buildType(DslTesting_BuildTypes(
             "feature_1",
             "1_This build is used for importing the json values",
-            "json_destination.json")),
+            "json_destination.json"))
 
-        buildType(DslTesting_BuildTypes(
+    val feature2 = buildType(DslTesting_BuildTypes(
             "feature_2",
             "2_This build is used for importing the json values",
-            "json_destination.json")),
+            "json_destination.json"))
 
-        buildType(DslTesting_BuildTypes(
+    val feature3 = buildType(DslTesting_BuildTypes(
             "feature_3",
             "3_This build is used for importing the json values",
-            "json_destination.json")),
+            "json_destination.json"))
 
-        buildType(DslTesting_BuildTypes(
+    val feature4 = buildType(DslTesting_BuildTypes(
             "feature_4",
             "4_This build is used for importing the json values",
-            "json_destination.json")),
+            "json_destination.json"))
 
-        buildType(DslTesting_BuildTypes(
+    val feature5 = buildType(DslTesting_BuildTypes(
             "feature_5",
             "5_This build is used for importing the json values",
-            "json_destination.json")),
+            "json_destination.json"))
 
-        buildType(DslTesting_BuildTypes(
+    val feature6 = buildType(DslTesting_BuildTypes(
             "feature_6",
             "6_This build is used for importing the json values",
-            "json_destination.json")),
+            "json_destination.json"))
 
-        buildType(DslTesting_BuildTypes(
+    val feature7 = buildType(DslTesting_BuildTypes(
             "feature_7",
             "7_This build is used for importing the json values",
-            "json_destination.json")),
+            "json_destination.json"))
 
-        buildType(DslTesting_BuildTypes(
+    val feature8 = buildType(DslTesting_BuildTypes(
             "feature_8",
             "8_This build is used for importing the json values",
-            "json_destination.json")),
+            "json_destination.json"))
 
-        buildType(DslTesting_BuildTypes(
+    val feature9 = buildType(DslTesting_BuildTypes(
             "feature_9",
             "9_This build is used for importing the json values",
             "json_destination.json"))
-    )
-
-//    val featureList = listOf<String>("feature1", "feature2", "feature3", "feature4", "feature5", "feature6", "feature7", "feature8", "feature9")
 
 
 
+//
+    val featureList = listOf<String>("feature1", "feature2", "feature3", "feature4", "feature5", "feature6", "feature7", "feature8", "feature9")
+//
+//
+//
+//
+//    sequential {
+//        featureList.forEach { feature ->
+//            buildType(feature, "json_destination.json", "python_runner", "main.py", "--path_to_source_json_file %source% --path_to_destination_json_file %destination% --location_one %location1% --location_two %location2%")
+//        }
+//    }
+//
 //    featureList.last().triggers {
 //        vcs {
-//            root(DslTesting_HttpsGithubComAjayKumar9375dslTestingGitRefsHeadsMain)
-//            triggerRules = " +:.*" // trigger the build on any change in the VCS root
 //        }
 //    }
 
 
+//
+//    featureList.forEach{ buildType()}
 // featureList.last().triggers{
 //        vcs {
 //        }
