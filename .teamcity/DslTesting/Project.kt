f07bf793fd960b52cf151e62a5270291435a2e41
Ajay Kumar <ajaya9375@gmail.com>
2024-06-14 12:11:30 +0530

    new

--- .teamcity/DslTesting/Project.kt
+++ .teamcity/DslTesting/Project.kt
@@ -88,47 +88,47 @@ object Project : Project({
     //Features List
 
     val feature1 = buildType(DslTesting_BuildTypes(
-            "feature1",
+            "feature_1",
             "1_This build is used for importing the json values",
             "json_destination.json"))
 
     val feature2 = buildType(DslTesting_BuildTypes(
-            "feature2",
+            "feature_2",
             "2_This build is used for importing the json values",
             "json_destination.json"))
 
     val feature3 = buildType(DslTesting_BuildTypes(
-            "feature3",
+            "feature_3",
             "3_This build is used for importing the json values",
             "json_destination.json"))
 
     val feature4 = buildType(DslTesting_BuildTypes(
-            "feature4",
+            "feature_4",
             "4_This build is used for importing the json values",
             "json_destination.json"))
 
     val feature5 = buildType(DslTesting_BuildTypes(
-            "feature5",
+            "feature_5",
             "5_This build is used for importing the json values",
             "json_destination.json"))
 
     val feature6 = buildType(DslTesting_BuildTypes(
-            "feature6",
+            "feature_6",
             "6_This build is used for importing the json values",
             "json_destination.json"))
 
     val feature7 = buildType(DslTesting_BuildTypes(
-            "feature7",
+            "feature_7",
             "7_This build is used for importing the json values",
             "json_destination.json"))
 
     val feature8 = buildType(DslTesting_BuildTypes(
-            "feature8",
+            "feature_8",
             "8_This build is used for importing the json values",
             "json_destination.json"))
 
     val feature9 = buildType(DslTesting_BuildTypes(
-            "feature9",
+            "feature_9",
             "9_This build is used for importing the json values",
             "json_destination.json"))
 
