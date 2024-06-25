d9119548c23f34cf849a3842e54a8afcd10fe67e
Ajay Kumar <ajaya9375@gmail.com>
2024-06-25 12:00:06 +0530

    changes

--- .teamcity/DslTesting/DslBuildStep/dslBuildStep.kt
+++ .teamcity/DslTesting/DslBuildStep/dslBuildStep.kt
@@ -11,15 +11,14 @@ import jetbrains.buildServer.configs.kotlin.buildSteps.*
 
 // })
 
-fun createpythonStep(scriptArg: String, fileName: String) = buildSteps {
-     step(python{
-        // id = "python_runner"
-        parameter("id", "Python_runner")
-        // command = file {
-            // filename = fileName
-            // scriptArguments = scriptArg
-            parameter("filename", fileName)
-            parameter("scriptArguments", scriptArg)
+fun createpythonStep(scriptArg: String, fileName: String) = python{
+        id = "python_runner"
+        // parameter("id", "Python_runner")
+        command = file {
+            filename = fileName
+            scriptArguments = scriptArg
+            // parameter("filename", fileName)
+            // parameter("scriptArguments", scriptArg)
         // }
-        })
+        }
 }
