d9acebe5e1a2f47815b78cec24bd813cab09190a
Ajay Kumar <ajaya9375@gmail.com>
2024-06-25 14:46:04 +0530

    changes

--- .teamcity/DslTesting/DslBuildStep/dslBuildStep.kt
+++ .teamcity/DslTesting/DslBuildStep/dslBuildStep.kt
@@ -4,6 +4,18 @@ import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.python
 import jetbrains.buildServer.configs.kotlin.buildSteps.*
 
+import jetbrains.buildServer.configs.kotlin.buildSteps.python.PythonCommand
+import jetbrains.buildServer.configs.kotlin.buildSteps.python.File
+
+fun createPythonStep(): python {
+    return python {
+        id = "python_runner"
+        command = PythonCommand.File {
+            filename = "main.py"
+            scriptArguments = "--path_to_source_json_file %source% --path_to_destination_json_file %destination% --location_one %location1% --location_two %location2%"
+        }
+    }
+}
 
 // class DslTesting_BuildStep(
 //     id: String, 
@@ -20,12 +32,12 @@ import jetbrains.buildServer.configs.kotlin.buildSteps.*
 //         }
 // }
 
-fun createPythonStep(): BuildStep {
-    return python {
-        id = "python_runner"
-        command = file {
-            filename = "main.py"
-            scriptArguments = "--path_to_source_json_file %source% --path_to_destination_json_file %destination% --location_one %location1% --location_two %location2%"
-        }
-    }
-}
+// fun createPythonStep(): BuildStep {
+//     return python {
+//         id = "python_runner"
+//         command = file {
+//             filename = "main.py"
+//             scriptArguments = "--path_to_source_json_file %source% --path_to_destination_json_file %destination% --location_one %location1% --location_two %location2%"
+//         }
+//     }
+// }
