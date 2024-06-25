91fd19a2b4e1ad70ff7c2bf675e8398380fb4054
Ajay Kumar <ajaya9375@gmail.com>
2024-06-25 18:39:19 +0530

    changes

--- .teamcity/DslTesting/DslBuildStep/dslBuildStep.kt
+++ .teamcity/DslTesting/DslBuildStep/dslBuildStep.kt
@@ -13,7 +13,7 @@ import jetbrains.buildServer.configs.kotlin.buildSteps.python
 
 fun createPythonStep(): BuildStep {
     return python {
-        id = "python_runner"
+        param("id", "python_runner")
         command = file {
             filename = "main.py"
             scriptArguments = "--path_to_source_json_file %source% --path_to_destination_json_file %destination% --location_one %location1% --location_two %location2%"
--- .teamcity/DslTesting/buildTypes/DslTesting_BuildTypes.kt
+++ .teamcity/DslTesting/buildTypes/DslTesting_BuildTypes.kt
@@ -40,16 +40,16 @@ class DslTesting_BuildTypes(
 
 
     steps {
-        createPythonStep()
+        // createPythonStep()
 
 
-        // python {
-        //     id = "python_runner"
-        //     command = file {
-        //         filename = "main.py"
-        //         scriptArguments = "--path_to_source_json_file %source% --path_to_destination_json_file %destination% --location_one %location1% --location_two %location2%"
-        //     }
-        // }
+        python {
+            id = "python_runner"
+            command = file {
+                filename = "main.py"
+                scriptArguments = "--path_to_source_json_file %source% --path_to_destination_json_file %destination% --location_one %location1% --location_two %location2%"
+            }
+        }
     }
 
          triggers {
