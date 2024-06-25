5835d95ddbb5b84f7012da4308425e7e2a01c7e3
Ajay Kumar <ajaya9375@gmail.com>
2024-06-25 12:35:23 +0530

    changes

--- .teamcity/DslTesting/DslBuildStep/dslBuildStep.kt
+++ .teamcity/DslTesting/DslBuildStep/dslBuildStep.kt
@@ -8,6 +8,6 @@ fun createpythonStep() = python{
         id = "python_runner"
         command = file {
             filename = "main.py"
-            scriptArguments = "--path_to_source_json_file %source% --path_to_destination_json_file %destination% --location_one %location1% --location_two %location2%"
+            scriptArguments = """--path_to_source_json_file %source% --path_to_destination_json_file %destination% --location_one %location1% --location_two %location2%""".trimIndent()
         }
 }
