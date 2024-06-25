f26bda2f6b9b935f0a2d6b530a3487f98d8f0d27
Ajay Kumar <ajaya9375@gmail.com>
2024-06-25 14:04:27 +0530

    changes

--- .teamcity/DslTesting/DslBuildStep/dslBuildStep.kt
+++ .teamcity/DslTesting/DslBuildStep/dslBuildStep.kt
@@ -1,20 +1,24 @@
 package DslTesting.DslBuildStep
 
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.python
-// import jetbrains.buildServer.configs.kotlin.buildSteps.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.*
+// class DslTesting_BuildStep(
+//     id: String, 
+//     filename: String, 
+//     scriptArguments: String) : BuildStep({
+// })
 
-// fun createpythonStep() = python{
+// fun createpythonStep()
+//     python{
 //         id = "python_runner"
 //         command = file {
 //             filename = "main.py"
-//             scriptArguments = """--path_to_source_json_file %source% --path_to_destination_json_file %destination% --location_one %location1% --location_two %location2%""".trimIndent()
+//             scriptArguments = "--path_to_source_json_file %source% --path_to_destination_json_file %destination% --location_one %location1% --location_two %location2%"
 //         }
 // }
 
-fun createPythonStep(): buildSteps {
+fun createPythonStep(): BuildStep {
     return python {
         id = "python_runner"
         command = file {
